7116c041a1207ae708048f0926ddb5729176386e
Nguyen Trong Dai <n15dccn062@student.ptithcm.edu.vn>
2018-12-15 14:42:05 +0700

    Update

--- TTN_PT/FormNhapNgay.Designer.cs
+++ TTN_PT/FormNhapNgay.Designer.cs
@@ -30,132 +30,95 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.txtTuNgay = new DevExpress.XtraEditors.DateEdit();
-            this.txtDenNgay = new DevExpress.XtraEditors.DateEdit();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            ((System.ComponentModel.ISupportInitialize)(this.txtTuNgay.Properties.CalendarTimeProperties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.txtTuNgay.Properties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.txtDenNgay.Properties.CalendarTimeProperties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.txtDenNgay.Properties)).BeginInit();
-            this.groupBox1.SuspendLayout();
+            this.dateTuNgay = new DevExpress.XtraEditors.DateEdit();
+            this.dateDenNgay = new DevExpress.XtraEditors.DateEdit();
+            this.btnIn = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dateTuNgay.Properties.CalendarTimeProperties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateTuNgay.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateDenNgay.Properties.CalendarTimeProperties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateDenNgay.Properties)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(28, 54);
-            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Location = new System.Drawing.Point(44, 27);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(81, 21);
+            this.label1.Size = new System.Drawing.Size(54, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Từ ngày :";
+            this.label1.Text = "Từ ngày: ";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(28, 113);
-            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Location = new System.Drawing.Point(44, 72);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(90, 21);
+            this.label2.Size = new System.Drawing.Size(54, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Đến ngày :";
+            this.label2.Text = "Đến ngày";
             // 
-            // txtTuNgay
+            // dateTuNgay
             // 
-            this.txtTuNgay.EditValue = null;
-            this.txtTuNgay.Location = new System.Drawing.Point(186, 51);
-            this.txtTuNgay.Margin = new System.Windows.Forms.Padding(5);
-            this.txtTuNgay.Name = "txtTuNgay";
-            this.txtTuNgay.Properties.Appearance.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtTuNgay.Properties.Appearance.Options.UseFont = true;
-            this.txtTuNgay.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            this.dateTuNgay.EditValue = null;
+            this.dateTuNgay.Location = new System.Drawing.Point(137, 27);
+            this.dateTuNgay.Name = "dateTuNgay";
+            this.dateTuNgay.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
             new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
-            this.txtTuNgay.Properties.CalendarTimeProperties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            this.dateTuNgay.Properties.CalendarTimeProperties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
             new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
-            this.txtTuNgay.Properties.CalendarTimeProperties.DisplayFormat.FormatType = DevExpress.Utils.FormatType.Numeric;
-            this.txtTuNgay.Properties.CalendarTimeProperties.EditFormat.FormatType = DevExpress.Utils.FormatType.Numeric;
-            this.txtTuNgay.Properties.DisplayFormat.FormatString = "";
-            this.txtTuNgay.Properties.DisplayFormat.FormatType = DevExpress.Utils.FormatType.Numeric;
-            this.txtTuNgay.Properties.EditFormat.FormatString = "";
-            this.txtTuNgay.Properties.EditFormat.FormatType = DevExpress.Utils.FormatType.Numeric;
-            this.txtTuNgay.Size = new System.Drawing.Size(233, 28);
-            this.txtTuNgay.TabIndex = 2;
-            this.txtTuNgay.EditValueChanged += new System.EventHandler(this.dateEdit1_EditValueChanged);
+            this.dateTuNgay.Size = new System.Drawing.Size(198, 20);
+            this.dateTuNgay.TabIndex = 2;
             // 
-            // txtDenNgay
+            // dateDenNgay
             // 
-            this.txtDenNgay.EditValue = null;
-            this.txtDenNgay.Location = new System.Drawing.Point(186, 110);
-            this.txtDenNgay.Margin = new System.Windows.Forms.Padding(5);
-            this.txtDenNgay.Name = "txtDenNgay";
-            this.txtDenNgay.Properties.Appearance.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtDenNgay.Properties.Appearance.Options.UseFont = true;
-            this.txtDenNgay.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            this.dateDenNgay.EditValue = null;
+            this.dateDenNgay.Location = new System.Drawing.Point(137, 69);
+            this.dateDenNgay.Name = "dateDenNgay";
+            this.dateDenNgay.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
             new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
-            this.txtDenNgay.Properties.CalendarTimeProperties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            this.dateDenNgay.Properties.CalendarTimeProperties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
             new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
-            this.txtDenNgay.Properties.DisplayFormat.FormatString = "";
-            this.txtDenNgay.Properties.DisplayFormat.FormatType = DevExpress.Utils.FormatType.Numeric;
-            this.txtDenNgay.Size = new System.Drawing.Size(233, 28);
-            this.txtDenNgay.TabIndex = 3;
+            this.dateDenNgay.Size = new System.Drawing.Size(198, 20);
+            this.dateDenNgay.TabIndex = 3;
             // 
-            // button1
+            // btnIn
             // 
-            this.button1.Location = new System.Drawing.Point(186, 170);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 53);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "IN";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnIn.Location = new System.Drawing.Point(137, 130);
+            this.btnIn.Name = "btnIn";
+            this.btnIn.Size = new System.Drawing.Size(75, 23);
+            this.btnIn.TabIndex = 4;
+            this.btnIn.Text = "In";
+            this.btnIn.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnThoat
             // 
-            this.button2.Location = new System.Drawing.Point(319, 170);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 53);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "EXIT";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.txtTuNgay);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.button2);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.txtDenNgay);
-            this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox1.Location = new System.Drawing.Point(0, 0);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(495, 292);
-            this.groupBox1.TabIndex = 6;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
-            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            this.btnThoat.Location = new System.Drawing.Point(275, 130);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 5;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
             // 
             // FormNhapNgay
             // 
-            this.Appearance.Options.UseFont = true;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(495, 292);
-            this.Controls.Add(this.groupBox1);
-            this.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(5);
+            this.ClientSize = new System.Drawing.Size(506, 229);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnIn);
+            this.Controls.Add(this.dateDenNgay);
+            this.Controls.Add(this.dateTuNgay);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "FormNhapNgay";
             this.Text = "FormNhapNgay";
-            ((System.ComponentModel.ISupportInitialize)(this.txtTuNgay.Properties.CalendarTimeProperties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.txtTuNgay.Properties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.txtDenNgay.Properties.CalendarTimeProperties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.txtDenNgay.Properties)).EndInit();
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dateTuNgay.Properties.CalendarTimeProperties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateTuNgay.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateDenNgay.Properties.CalendarTimeProperties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateDenNgay.Properties)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -163,10 +126,9 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private DevExpress.XtraEditors.DateEdit txtTuNgay;
-        private DevExpress.XtraEditors.DateEdit txtDenNgay;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.GroupBox groupBox1;
+        private DevExpress.XtraEditors.DateEdit dateTuNgay;
+        private DevExpress.XtraEditors.DateEdit dateDenNgay;
+        private System.Windows.Forms.Button btnIn;
+        private System.Windows.Forms.Button btnThoat;
     }
 }
